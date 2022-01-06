07baf0a2b4c1960a9906d2ce4de74cac458c1579
Lindsay Ward <lward23@hotmail.com>
2022-01-06 08:41:10 -0800

    Add axios and "like" animated heart

--- src/Photo.jsx
+++ src/Photo.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import Heart from "react-animated-heart";
 
 export default function Photo() {
+  const [isClick, setClick] = useState(false);
   return (
-    <article>
+    <article className="Photo">
       <h2>Hello from Photo</h2>
+      <button>
+        <Heart isClick={isClick} onClick={() => setClick(!isClick)} />
+      </button>
     </article>
   );
 }
+
+// API Key is ptWGI0lwo6zmVqleMkST6V9CfElUnCJggQwDRgWs
