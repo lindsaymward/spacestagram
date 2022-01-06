b8eb128e7bde06de488c60c8ecc4c9e0d0f76d89
Lindsay Ward <lward23@hotmail.com>
2022-01-05 19:11:15 -0800

    Setup initial file structure and remove default styling and content from create-react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Photo from "./Photo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Spacestragram</h1>
       </header>
+      <main>
+        <Photo />
+      </main>
+      <footer>
+        <p>Coded by Lindsay Ward</p>
+      </footer>
     </div>
   );
 }
---
+++ src/Photo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Photo() {
+  return (
+    <article>
+      <h2>Hello from Photo</h2>
+    </article>
+  );
+}
