ff88268aff716b078b3cbed29a8bee69421f975a
Lindsay Ward <lward23@hotmail.com>
2022-01-06 22:58:36 -0800

    Remove animation for better control over sizing of button. Change text of button upon click.

--- src/Photo.jsx
+++ src/Photo.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
-import Heart from "react-animated-heart";
 
 export default function Photo() {
-  const [isClick, setClick] = useState(false);
+  const [like, setLike] = useState("Like");
+
+  function LikePhoto() {
+    like === "Like" ? setLike("Unlike") : setLike("Like");
+  }
+
   return (
     <article className="Photo">
       <h2>Hello from Photo</h2>
-      <button>
-        <Heart isClick={isClick} onClick={() => setClick(!isClick)} />
-      </button>
+      <button onClick={LikePhoto}>{like}</button>
     </article>
   );
 }
