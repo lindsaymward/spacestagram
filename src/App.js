697deaf1473ad38bf68b979783cc1225cb688caa
Lindsay Ward <lward23@hotmail.com>
2022-01-08 16:17:56 -0800

    Move API call to App to allow for several Photo components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 import Photo from "./Photo.jsx";
 
 function App() {
+  let [imageUrl, setImageUrl] = useState("");
+  let [imageTitle, setImageTitle] = useState("");
+
+  function showPhoto(response) {
+    console.log(response);
+    setImageUrl(response.data.hdurl);
+    setImageTitle(response.data.title);
+  }
+
+  useEffect(() => {
+    const apiKey = "ptWGI0lwo6zmVqleMkST6V9CfElUnCJggQwDRgWs";
+    let apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+    axios.get(apiUrl).then(showPhoto);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Spacestagram</h1>
       </header>
       <main>
-        <Photo />
+        <Photo url={imageUrl} title={imageTitle} />
       </main>
       <footer>
         <p>Coded by Lindsay Ward</p>
--- src/Photo.jsx
+++ src/Photo.jsx
@@ -1,28 +1,12 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import Like from "./Like.jsx";
 import "./Photo.css";
 
-export default function Photo() {
-  let [load, setLoad] = useState(false);
-  let [imageUrl, setImageUrl] = useState(null);
-
-  function showPhoto(response) {
-    console.log(response);
-    setImageUrl(response.data.hdurl);
-    setLoad(true);
-  }
-
-  useEffect(() => {
-    const apiKey = "ptWGI0lwo6zmVqleMkST6V9CfElUnCJggQwDRgWs";
-    let apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
-    axios.get(apiUrl).then(showPhoto);
-  }, []);
-
-  if (load) {
+export default function Photo(props) {
+  if (props.url) {
     return (
       <article className="Photo">
-        <img src={imageUrl} alt="NASA" />
+        <img src={props.url} alt={props.title} />
         <Like />
       </article>
     );
