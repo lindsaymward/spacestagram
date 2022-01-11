f68f5d7ed6e39c31bab40a514a6742d8a219b124
Lindsay Ward <lward23@hotmail.com>
2022-01-10 22:17:19 -0800

    Add links to Github repository and LinkedIn profile to footer

--- src/App.js
+++ src/App.js
@@ -43,7 +43,20 @@ function App() {
         <main>{photo}</main>
         <footer>
           <p>Coded by Lindsay Ward</p>
-          <i class="bi-github" role="img" aria-label="GitHub"></i>
+          <a
+            href="https://github.com/lindsaymward/spacestagram"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="bi-github" role="img" aria-label="GitHub"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/lindsaymward21/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="bi bi-linkedin"></i>
+          </a>
         </footer>
       </div>
     );
